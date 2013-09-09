a3b9c62b3bcf9586c82299f5a72de1e3d88f3da8
Al Snow <jasnow@hotmail.com>
2013-09-09 15:26:05 -0400

    Refreshed Gemfile.lock file; Added secret to config/initializers/devise.rb file

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,4 +13,6 @@ Devise.setup do |config|
 
   # rememberable
   config.remember_for = 12.weeks
+
+  config.secret_key = '5a9aa9a81bc943f85d910c859306b7773513cdfd2262204db93832ba89715005874f8d51db33cf8d7ddd6f3a5c72c71066b3fed8b3d09f016eb2c96ead7c5cae'
 end
